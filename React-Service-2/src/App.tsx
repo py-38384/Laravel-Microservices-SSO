04d7c9836a3ac387d267c2669e1b2f503120364f
Piyal Hossein <piyal13133@gmail.com>
2025-08-27 23:48:27 +0600

    React Service Fix

--- React-Service-2/src/App.tsx
+++ React-Service-2/src/App.tsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import Spinner from './components/Spinner.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [authenticated, setAuthenticated] = useState<boolean>((function (){
+    const isAutenticated = localStorage.getItem('auth');
+    if(isAutenticated === 'authenticated'){
+      return true
+    } else {
+      return false
+    }
+  })());
+  const [loading, setLoading] = useState<boolean>(true);
+  const setAuth = async () => {
+    const res = await fetch("http://sso.microservice.test:8000/check-auth",{
+        credentials: "include",
+    })
+    const data = await res.json()
+    console.log(data)
+    if(data?.authenticated){
+        setAuthenticated(true)
+        localStorage.setItem('auth', 'authenticated')
+      } else {
+        setAuthenticated(false)
+        localStorage.removeItem('auth')
+    }
+    setLoading(false);
+  }
+  const gotoDashboard = () => {
+    location.href = "http://sso.microservice.test:8000/dashboard"
+  }
+  const handleLogout = async () => {
+    location.href = "http://sso.microservice.test:8000/token-logout"
+  }
+  const gotoLogin = async () => {
+    location.href = `http://sso.microservice.test:8000/login?next=http://service1.microservice.test:8000/`
+  }
+  useEffect(() => {
+    setAuth()
 
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {loading? (
+      <div><Spinner show={true}/></div>
+    ): (
+      <>
+      {authenticated? (
+        <div className='mb-12'>
+        <button className='mr-3' onClick={gotoDashboard}>Dashboard</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
+      ): (
+        <div className='mb-12'>
+        <button className='mr-3' onClick={gotoLogin}>Login</button>
+        <button>Register</button>
+      </div>
+      )}
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      </>
+    )}
     </>
   )
 }
